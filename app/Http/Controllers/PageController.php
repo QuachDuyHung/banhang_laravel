bd09a24cc050127be38e6574f42d5d7ac52b7048
= <=>
2019-11-22 15:07:08 +0700

    first commit

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Slide;
+use App\Product;
+use App\ProductType;
+use App\Cart;
+use Session;
+
+class PageController extends Controller
+{
+    public function getIndex(){
+        $slide = Slide::all();
+        // return view('pages.trangchu',['slide'=>$slide]);
+        $new_product = Product::where('new',1)->paginate(4);
+        $sanphamkhuyenmai = Product::where('promotion_price','<>',0)->paginate(8);
+        return view('pages.trangchu', compact('slide','new_product', 'sanphamkhuyenmai'));
+    }
+
+    public function getLoaiSp($type){
+        $sp_theoloai = Product::where('id_type',$type)->get();
+        $sp_khac = Product::where('id_type','<>',$type)->paginate(3);
+        $loai = ProductType::all();
+        $loai_sp = ProductType::where('id',$type)->first();
+        return view('pages.loai_sanpham', compact('sp_theoloai','sp_khac','loai','loai_sp'));
+    }
+
+    public function getchiTiet(Request $req){
+        $sanpham = Product::where('id',$req->id)->first();
+        $sp_tuongtu = Product::where('id_type',$sanpham->id_type)->paginate(6);
+        return view('pages.chitiet_sanpham',compact('sanpham','sp_tuongtu'));
+    }
+
+    public function getLienhe(){
+        return view('pages.lien_he');
+    }
+
+    public function getGioithieu(){
+        return view('pages.gioi_thieu');
+    }
+    //them gio hang
+    public function getAddtoCart(Request $req, $id){
+        $product = Product::find($id);
+        $oldCart = Session('cart')?Session::get('cart'):null;
+        $cart = new Cart($oldCart);
+        $cart->add($product, $id);
+        $req->session()->put('cart',$cart);
+        return redirect()->back();
+    }
+    //xoa gio hang
+    public function getDelitemCart($id){
+        $oldCart = Session::has('cart') ? Session::get('cart') : null;
+        $cart = new Cart($oldCart);
+        $cart->removeItem($id);
+        if(count($cart->items) > 0){
+            Session::put('cart', $cart);
+        }
+        else{
+            Session::forget('cart');
+        }
+        Session::put('cart', $cart);
+        return redirect()->back();
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('index',[
+    'as'=>'trang-chu',
+    'uses'=>'PageController@getIndex'
+]);
+Route::get('loai-san-pham/{type}',[
+    'as'=>'loaisanpham',
+    'uses'=>'PageController@getLoaiSp'
+]);
+Route::get('chi-tiet-san-pham/{id}',[
+    'as'=>'chitietsanpham',
+    'uses'=>'PageController@getchiTiet'
+]);
+Route::get('lien-he',[
+    'as'=>'lienhe',
+    'uses'=>'PageController@getLienhe'
+]);
+Route::get('gioi-thieu',[
+    'as'=>'gioithieu',
+    'uses'=>'PageController@getGioithieu'
+]);
+
+//thêm vào giỏ hàng
+Route::get('add-to-cart/{id}',[
+    'as'=>'themgiohang',
+    'uses'=>'PageController@getAddtoCart'
+]);
+
+//xóa giỏ hàng
+Route::get('del-cart/{id}',[
+    'as'=>'xoagiohang',
+    'uses'=>'PageController@getDelitemCart'
+]);
